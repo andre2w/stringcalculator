2f440a8ce6c8809c0eec958d53192af704f11a7f
Andre Torres <andre.torres@codurance.com>
2018-09-11 16:01:15 +0100

    handling single number

--- src/main/java/com/codurance/andre/StringCalculator.java
+++ src/main/java/com/codurance/andre/StringCalculator.java
@@ -1,7 +1,14 @@
 package com.codurance.andre;
 
+import static java.lang.Integer.parseInt;
+
 public class StringCalculator {
     public int add(String operation) {
+
+        if (operation.length() == 1) {
+            return parseInt(operation);
+        }
+
         return 0;
     }
 }
--- src/test/java/com/codurance/andre/StringCalculatorShould.java
+++ src/test/java/com/codurance/andre/StringCalculatorShould.java
@@ -1,5 +1,6 @@
 package com.codurance.andre;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import javax.swing.*;
@@ -8,12 +9,25 @@ import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorShould {
 
+    private StringCalculator stringCalculator;
+
+    @Before
+    public void setUp() throws Exception {
+        stringCalculator = new StringCalculator();
+    }
+
     @Test
     public void return_zero_for_empty_string() {
-        StringCalculator stringCalculator = new StringCalculator();
-
         int result = stringCalculator.add("");
 
         assertEquals(result, 0);
+    }
+
+    @Test
+    public void return_number_when_only_one_number_is_passed() {
+
+        int result = stringCalculator.add("1");
+
+        assertEquals(result, 1);
     }
 }
